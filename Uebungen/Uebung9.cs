2cea69916403d7486316184fa18624f8317cc71e
schlosserleo <gymnasium.ottweiler@gmail.com>
2023-05-12 19:20:29 +0200

    10/10 9

--- Uebungen/Uebung9.cs
+++ Uebungen/Uebung9.cs
@@ -5,14 +5,13 @@ public static class Uebung9
 {
     public static void MethodenParameter()
     {
-        var text1 = "Halloo";
-        var text2 = "Hallo";
+        var text1 = "Hochschule für Technik und Wirtschaft";
+        var text2 = "HTW Berlin";
         Console.WriteLine($"Text 1: '{text1}' \nText 2: '{text2} ");
-        Sort(ref text1, ref text2);
-        Console.WriteLine($"Text 1: '{text1}' \nText 2: '{text2} ");
+        if (Sort(ref text1, ref text2))
+            Console.WriteLine($"Text 1: '{text1}' \nText 2: '{text2} ");
         GetCurrentDate(out var day, out var month, out var year);
         Console.WriteLine($"{day}, {month}, {year}");
-        //GetCurrentDate2(12, 05, 2023);
     }
     
     public static void GetCurrentDate(out int day, out int month, out int year)
@@ -25,11 +24,10 @@ public static class Uebung9
     public static bool Sort(ref string text1, ref string text2)
     {
         if (text1 == "" || text2 == "") return false;
-        if (text1.Length > text2.Length)
+        if (string.Compare(text1, text2, StringComparison.InvariantCulture) > 0)
         {
             (text1, text2) = (text2, text1);
         }
-
         return true;
     }
 }
