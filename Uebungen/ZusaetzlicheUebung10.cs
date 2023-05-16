2aecda1c7e6fa55fe76d4c63e04fa3c995f6fbc4
schlosserleo <gymnasium.ottweiler@gmail.com>
2023-05-16 10:38:40 +0200

    10z

--- Uebungen/ZusaetzlicheUebung10.cs
+++ Uebungen/ZusaetzlicheUebung10.cs
@@ -2,5 +2,24 @@ namespace Programmieren_I.Uebungen;
 
 public class ZusaetzlicheUebung10
 {
-    
+    public static void BisektionsVerfahren()
+    {
+        
+    }
+
+    public static double ContinousFunction(double x)
+    {
+        return Math.Pow(x, 3) + x - 5;
+    }
+
+    public static double CalculateRootOfPolynomial(double a, double b)
+    {
+        var midPoint = double.NaN;
+        var maxTolerance = Math.Pow(10, -6)
+        for (var i = 1; i <= 1000; i++)
+        {
+            if (!(a < b)) return midPoint;
+            
+        }
+    }
 }
