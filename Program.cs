415e640ed7924127a71df870f05409fb80b1effd
schlosserleo <gymnasium.ottweiler@gmail.com>
2023-05-17 12:58:23 +0200

    Cleanup

--- Program.cs
+++ Program.cs
@@ -6,13 +6,11 @@ internal static class ProgrammierenI
 {
     private static void Main()
     {
-        int lineInTxtFile;
         while (true)
         {
-            Console.WriteLine("Sie haben folgende Programme zur Auswahl(Eingabe nach Schema: 1/2/2z/3z)");
-            PrintAppList(); 
+            Console.WriteLine("Sie haben folgende Programme zur Auswahl:");
+            PrintAppList();
             var shortAppName = GetUserInput();
-            
 
             if (LaunchApp(shortAppName))
                 break;
@@ -42,6 +40,7 @@ internal static class ProgrammierenI
 
     private static string GetUserInput()
     {
+        Console.Write("Eingabe nach Schema: 1/2/2z/3z: ");
         var selection = Console.ReadLine() ?? throw new InvalidOperationException();
         return selection;
     }
@@ -92,81 +91,6 @@ internal static class ProgrammierenI
         }
     }
 
-    /*private static bool LaunchApp(string shortAppName)
-    {
-        switch (shortAppName)
-        {
-            case "1":
-                Uebung1.Anfaenge();
-                return true;
-            case "1z":
-                ZusaetzlicheUebung1.Rechner();
-                return true;
-            case "2":
-                Uebung2.Eingaben();
-                return true;
-            case "2z":
-                ZusaetlicheUebung2.Logikaufgabe();
-                return true;
-            case "3":
-                Uebung3.Typen();
-                return true;
-            case "3z":
-                ZusaetzlicheUebung3.AusgabeAlsBinärUndHexadezimalZahl();
-                return true;
-            case "4":
-                Uebung4.MathematischeFunktionen();
-                return true;
-            case "4z":
-                ZusaetzlicheUebung4.Zinsberechnung();
-                return true;
-            case "5":
-                Uebung5.AnsweisungenUmsetzen();
-                return true;
-            case "5z":
-                ZusaetzlicheUebung5.Fakultaet();
-                return true;
-            case "6":
-                Uebung6.ReihenBerechnen();
-                return true;
-            case "6z":
-                ZusaetzlicheUebung6.NewtonVerfahren();
-                return true;
-            case "7":
-                Uebung7.Textanalyse();
-                return true;
-            case "7z":
-                ZusaetzlicheUebung7.TextanalyseMitCollections();
-                return true;
-            case "8":
-                Uebung8.DatumBestimmen();
-                return true;
-            case "8z":
-                ZusaetzlicheUebung8.IterationUndRekursion();
-                return true;
-            case "9":
-                Uebung9.MethodenParameter();
-                return true;
-            case "9z":
-                ZusaetzlicheUebung9.PrimzahlenOderDasSiebDesEratosthenes();
-                return true;
-            case "10":
-                Uebung10.KlassenErstellenTeil1();
-                return true;
-            case "10z":
-                ZusaetzlicheUebung10.BisektionsVerfahren();
-                return true;
-            case "11":
-                Uebung11.KlassenErstellenTeil2();
-                return true;
-            case "11z":
-                ZusaetzlicheUebung11.ZahlenSortieren();
-                return true;
-            default:
-                Console.WriteLine("Keine valide Eingabe.\nBitte geben sie eine Programmnummer aus der Liste ein");
-                return false;
-        }
-    }*/
     private static bool LaunchApp(string shortAppName)
     {
         var appDictionary = new Dictionary<string, Action>
