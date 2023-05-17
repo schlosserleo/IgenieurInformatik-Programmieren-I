79d3b40491effbcaf145f41906f9c6d990881af7
schlosserleo <gymnasium.ottweiler@gmail.com>
2023-05-17 15:38:20 +0200

    Project Finished

--- Uebungen/ZusaetzlicheUebung11.cs
+++ Uebungen/ZusaetzlicheUebung11.cs
@@ -13,22 +13,30 @@ public static class ZusaetzlicheUebung11
     // ReSharper disable once MemberCanBePrivate.Global
     public static void GebeArrayAus(int[] zahlen)
     {
-        var j = 2;
-        foreach (var i in zahlen)
+        var numLine = string.Empty;
+        var maxNumLength = zahlen[^1].ToString().Length;
+        var lineCount = 0;
+
+        foreach (var item in zahlen.Select((value, i) => new { i, value }))
         {
-            var maxLines = Console.WindowHeight;
-            if (j == maxLines)
+            var value = item.value;
+            var i = item.i + 1;
+            numLine += value.ToString().PadLeft(maxNumLength) + " ";
+            if (i % 4 != 0) continue;
+
+            Console.WriteLine(numLine);
+            lineCount++;
+            if (lineCount == Console.WindowHeight - 2)
             {
                 Console.WriteLine("Bitte Enter für die nächste Seite drücken");
                 while (Console.ReadKey().Key != ConsoleKey.Enter)
                 {
                 }
 
-                j = 1;
+                lineCount = 0;
             }
 
-            Console.WriteLine($"{i}, ");
-            j++;
+            numLine = string.Empty;
         }
     }
 }
